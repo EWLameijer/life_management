f4c061303abcaa0c50bcd4a594690946e5ed707e
EWLameijer <eric_wubbo@hotmail.com>
2019-07-17 19:16:23 +0200

    Better model: contact is a person

--- contacts/admin.py
+++ contacts/admin.py
@@ -4,3 +4,9 @@ from . import models;
 # Register your models here.
 
 admin.site.register(models.Contact)
+admin.site.register(models.ContactType)
+admin.site.register(models.ContactMoment)
+admin.site.register(models.ConnectionType)
+admin.site.register(models.Person)
+admin.site.register(models.Connection)
+
--- contacts/models.py
+++ contacts/models.py
@@ -3,9 +3,62 @@ from django.db import models
 # Create your models here.
 
 NAME_LENGTH = 30
+DESCRIPTION_LENGTH = 20
+
+class Person(models.Model):
+    name = models.CharField(max_length=NAME_LENGTH, unique=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Contact(models.Model):
-    name = models.CharField(max_length=NAME_LENGTH)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.person.name
+
+
+class ContactType(models.Model):
+    description = models.CharField(max_length=DESCRIPTION_LENGTH)
+
+    def __str__(self):
+        return self.description
+
+
+class ContactMoment(models.Model):
+    name = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    date = models.DateField()
+    type = models.ForeignKey(ContactType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{}: {} (on {})".format(self.name, self.type, self.date)
+
+
+class ConnectionType(models.Model):
+    description = models.CharField(max_length=DESCRIPTION_LENGTH)
+
+    def __str__(self):
+        return self.description
+
+  
+class Connection(models.Model):
+    subject = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='subject')
+    relationship = models.ForeignKey(ConnectionType, on_delete=models.CASCADE)
+    other_person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='other')
+
+    def __str__(self):
+        return "{} - {} of {}".format(
+            self.subject.name,
+            self.relationship.description,
+            self.other_person.name)
+    
 
 
 
+
+
+
+
+    
+    
